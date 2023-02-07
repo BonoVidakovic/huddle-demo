35788daa942e4dce58764cd72f4e636fd875b8b4
bono <bono.vidakovic@protonmail.com>
2023-02-07 14:14:01 +0100

    Initialized project

---
+++ src/main/java/demo/huddle/huddledemo/HuddleDemoApplication.java
@@ -0,0 +1,13 @@
+package demo.huddle.huddledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuddleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuddleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/huddle/huddledemo/HuddleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.huddle.huddledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuddleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
